96af6d6a168a8d229e4f1bb67a8b44013b3a0b4f
housni2003 <rakotoari.housni@gmail.com>
2025-05-11 23:05:53 +0200

    ia2

--- src/modele/jeu/JoueurIA.java
+++ src/modele/jeu/JoueurIA.java
@@ -1,11 +1,17 @@
 package modele.jeu;
 
+import modele.jeu.pieces.Pion;
+import modele.jeu.pieces.Roi;
 import modele.plateau.Case;
 import modele.plateau.Plateau;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class JoueurIA extends Joueur {
+
+    private static final int PROFONDEUR_MAX = 4;
 
     public JoueurIA(Jeu jeu, Couleur couleur) {
         super(jeu, couleur);
@@ -13,29 +19,17 @@ public class JoueurIA extends Joueur {
 
     @Override
     public Coup getCoup() {
-        ArrayList<Coup> coupsPossibles = new ArrayList<>();
         Plateau plateau = jeu.getPlateau();
-
-        for (int x = 0; x < Plateau.SIZE_X; x++) {
-            for (int y = 0; y < Plateau.SIZE_Y; y++) {
-                Case c = plateau.getCases()[x][y];
-                if (c.getPiece() != null && c.getPiece().getCouleur() == couleur) {
-                    for (Case cible : c.getPiece().dCA.getCA()) {
-                        Coup coup = new Coup(c, cible);
-                        if (jeu.coupValide(coup)) {
-                            coupsPossibles.add(coup);
-                        }
-                    }
-                }
-            }
-        }
-
         Coup meilleurCoup = null;
         int meilleurScore = Integer.MIN_VALUE;
 
+        ArrayList<Coup> coupsPossibles = getCoupsPossibles(plateau, couleur);
+        Collections.shuffle(coupsPossibles);
+        coupsPossibles.sort(Comparator.comparingInt(c -> -prioriteCoup(plateau, c)));
+
         for (Coup coup : coupsPossibles) {
-            Plateau copie = plateau.simulerCoup(coup); // ✅ Simulation simple et propre
-            int score = evaluerPlateau(copie, couleur);
+            Plateau copie = plateau.simulerCoup(coup);
+            int score = minimax(copie, PROFONDEUR_MAX - 1, Integer.MIN_VALUE, Integer.MAX_VALUE, false);
             if (score > meilleurScore) {
                 meilleurScore = score;
                 meilleurCoup = coup;
@@ -45,24 +39,136 @@ public class JoueurIA extends Joueur {
         return meilleurCoup != null ? meilleurCoup : coupsPossibles.get(0);
     }
 
+    private int minimax(Plateau plateau, int profondeur, int alpha, int beta, boolean maximisant) {
+        Couleur couleurCourante = maximisant ? couleur : (couleur == Couleur.BLANC ? Couleur.NOIR : Couleur.BLANC);
+        if (profondeur == 0) {
+            return evaluerPlateau(plateau, couleur);
+        }
+
+        ArrayList<Coup> coups = getCoupsPossibles(plateau, couleurCourante);
+        coups.sort(Comparator.comparingInt(c -> -prioriteCoup(plateau, c)));
+
+        if (maximisant) {
+            int maxEval = Integer.MIN_VALUE;
+            for (Coup coup : coups) {
+                Plateau copie = plateau.simulerCoup(coup);
+                int eval = minimax(copie, profondeur - 1, alpha, beta, false);
+                maxEval = Math.max(maxEval, eval);
+                alpha = Math.max(alpha, eval);
+                if (beta <= alpha) break;
+            }
+            return maxEval;
+        } else {
+            int minEval = Integer.MAX_VALUE;
+            for (Coup coup : coups) {
+                Plateau copie = plateau.simulerCoup(coup);
+                int eval = minimax(copie, profondeur - 1, alpha, beta, true);
+                minEval = Math.min(minEval, eval);
+                beta = Math.min(beta, eval);
+                if (beta <= alpha) break;
+            }
+            return minEval;
+        }
+    }
+
+    private ArrayList<Coup> getCoupsPossibles(Plateau plateau, Couleur couleur) {
+        ArrayList<Coup> coupsPossibles = new ArrayList<>();
+        for (int x = 0; x < Plateau.SIZE_X; x++) {
+            for (int y = 0; y < Plateau.SIZE_Y; y++) {
+                Case c = plateau.getCases()[x][y];
+                if (c.getPiece() != null && c.getPiece().getCouleur() == couleur) {
+                    for (Case cible : c.getPiece().dCA.getCA()) {
+                        Coup coup = new Coup(c, cible);
+                        coupsPossibles.add(coup);
+                    }
+                }
+            }
+        }
+        return coupsPossibles;
+    }
 
     private int evaluerPlateau(Plateau plateau, Couleur couleurIA) {
         int score = 0;
+        int mobilite = 0;
+        int structurePion = 0;
+        int roiEnDanger = 0;
+
+        Case roiCase = trouverRoi(plateau, couleurIA);
+        Case roiOpposeCase = trouverRoi(plateau, couleurIA == Couleur.BLANC ? Couleur.NOIR : Couleur.BLANC);
+
         for (int x = 0; x < Plateau.SIZE_X; x++) {
             for (int y = 0; y < Plateau.SIZE_Y; y++) {
                 Piece p = plateau.getCases()[x][y].getPiece();
                 if (p != null) {
                     int valeur = switch (p.getClass().getSimpleName()) {
-                        case "Pion" -> 1;
-                        case "Cavalier", "Fou" -> 3;
-                        case "Tour" -> 5;
-                        case "Dame" -> 9;
+                        case "Pion" -> 100;
+                        case "Cavalier", "Fou" -> 320;
+                        case "Tour" -> 500;
+                        case "Dame" -> 900;
+                        case "Roi" -> 10000;
                         default -> 0;
                     };
-                    score += (p.getCouleur() == couleurIA) ? valeur : -valeur;
+
+                    int facteur = p.getCouleur() == couleurIA ? 1 : -1;
+                    score += facteur * valeur;
+
+                    if (p.dCA != null) {
+                        mobilite += facteur * p.dCA.getCA().size();
+                    }
+
+                    if (p instanceof Pion) {
+                        int avancee = (couleurIA == Couleur.BLANC) ? (7 - y) : y;
+                        structurePion += facteur * (avancee * 10);
+                    }
+
+                    if (p.dCA != null && p.getCouleur() == couleurIA && p.dCA.getCA().contains(roiOpposeCase)) {
+                        score += 500;
+                    }
                 }
             }
         }
-        return score;
+
+        if (roiCase != null) {
+            int protection = 0;
+            for (int dx = -1; dx <= 1; dx++) {
+                for (int dy = -1; dy <= 1; dy++) {
+                    if (dx == 0 && dy == 0) continue;
+                    Case voisine = plateau.getCaseRelative(roiCase, dx, dy);
+                    if (voisine != null && voisine.getPiece() != null && voisine.getPiece().getCouleur() == couleurIA) {
+                        protection++;
+                    }
+                }
+            }
+            roiEnDanger -= (8 - protection) * 30;
+        }
+
+        return score + mobilite * 5 + structurePion + roiEnDanger;
+    }
+
+    private Case trouverRoi(Plateau plateau, Couleur couleur) {
+        for (int x = 0; x < Plateau.SIZE_X; x++) {
+            for (int y = 0; y < Plateau.SIZE_Y; y++) {
+                Piece p = plateau.getCases()[x][y].getPiece();
+                if (p instanceof Roi && p.getCouleur() == couleur) {
+                    return plateau.getCases()[x][y];
+                }
+            }
+        }
+        return null;
+    }
+
+    private int prioriteCoup(Plateau plateau, Coup coup) {
+        Case cible = plateau.getCases()[coup.arr.x][coup.arr.y];
+        if (cible.getPiece() != null) {
+            String type = cible.getPiece().getClass().getSimpleName();
+            return switch (type) {
+                case "Pion" -> 1;
+                case "Cavalier", "Fou" -> 3;
+                case "Tour" -> 5;
+                case "Dame" -> 9;
+                default -> 0;
+            } * 100;
+        }
+        return 0;
     }
 }
--- src/modele/plateau/Plateau.java
+++ src/modele/plateau/Plateau.java
@@ -41,64 +41,42 @@ public class Plateau extends Observable implements Cloneable {
     private void placerPieces() {
         // Pièces noires
         tab[0][0].setPiece(new Tour(this, Couleur.NOIR));
-        tab[0][0].getPiece().setCase(tab[0][0]);
-
         tab[1][0].setPiece(new Cavalier(this, Couleur.NOIR));
-        tab[1][0].getPiece().setCase(tab[1][0]);
-
         tab[2][0].setPiece(new Fou(this, Couleur.NOIR));
-        tab[2][0].getPiece().setCase(tab[2][0]);
-
         tab[3][0].setPiece(new Dame(this, Couleur.NOIR));
-        tab[3][0].getPiece().setCase(tab[3][0]);
-
         tab[4][0].setPiece(new Roi(this, Couleur.NOIR));
-        tab[4][0].getPiece().setCase(tab[4][0]);
-
         tab[5][0].setPiece(new Fou(this, Couleur.NOIR));
-        tab[5][0].getPiece().setCase(tab[5][0]);
-
         tab[6][0].setPiece(new Cavalier(this, Couleur.NOIR));
-        tab[6][0].getPiece().setCase(tab[6][0]);
-
         tab[7][0].setPiece(new Tour(this, Couleur.NOIR));
-        tab[7][0].getPiece().setCase(tab[7][0]);
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 8; i++) {
             tab[i][1].setPiece(new Pion(this, Couleur.NOIR));
-            tab[i][1].getPiece().setCase(tab[i][1]);
         }
 
         // Pièces blanches
         tab[0][7].setPiece(new Tour(this, Couleur.BLANC));
-        tab[0][7].getPiece().setCase(tab[0][7]);
-
         tab[1][7].setPiece(new Cavalier(this, Couleur.BLANC));
-        tab[1][7].getPiece().setCase(tab[1][7]);
-
         tab[2][7].setPiece(new Fou(this, Couleur.BLANC));
-        tab[2][7].getPiece().setCase(tab[2][7]);
-
         tab[3][7].setPiece(new Dame(this, Couleur.BLANC));
-        tab[3][7].getPiece().setCase(tab[3][7]);
-
         tab[4][7].setPiece(new Roi(this, Couleur.BLANC));
-        tab[4][7].getPiece().setCase(tab[4][7]);
-
         tab[5][7].setPiece(new Fou(this, Couleur.BLANC));
-        tab[5][7].getPiece().setCase(tab[5][7]);
-
         tab[6][7].setPiece(new Cavalier(this, Couleur.BLANC));
-        tab[6][7].getPiece().setCase(tab[6][7]);
-
         tab[7][7].setPiece(new Tour(this, Couleur.BLANC));
-        tab[7][7].getPiece().setCase(tab[7][7]);
 
-        for (int i = 4; i < 8; i++) {
+        for (int i = 0; i < 8; i++) {
             tab[i][6].setPiece(new Pion(this, Couleur.BLANC));
-            tab[i][6].getPiece().setCase(tab[i][6]);
+        }
+
+        // Affecter les cases aux pièces
+        for (int x = 0; x < SIZE_X; x++) {
+            for (int y = 0; y < SIZE_Y; y++) {
+                if (tab[x][y].getPiece() != null) {
+                    tab[x][y].getPiece().setCase(tab[x][y]);
+                }
+            }
         }
     }
+
 
     public void notifierChangement() {
         setChanged();
